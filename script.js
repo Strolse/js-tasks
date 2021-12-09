fae5ae86ff13df00cf30fb2db1a16a6ec0573f2a
Olga Strekalovskaia <afalina_91@mail.ru>
2021-12-09 23:59:03 +0300

    создала проект

---
+++ script.js
@@ -0,0 +1,19 @@
+var name = 'Сергей';
+if (name == 'Сергей') {
+  console.log('да');
+}
+
+for (
+  var i = 0;
+  i < 3;
+  i++
+) {
+  console.log(i);
+}
+
+function sum(d, h) {
+  var result = d - h;
+  return result;
+}
+
+
